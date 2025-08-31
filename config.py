323df6035cca89df1dc19a769d814db1f3ca9a21
mosameer2098-ship-it <mosameer2098@gmail.com>
2025-08-31 20:21:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ARYANSONIOP/TestingMusicBot",
+    "https://github.com/mosameer2098-ship-it/IqraMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
@@ -50,7 +50,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tabahi_tabahi")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/tabahi_tabahi")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/best_friends_chatting_zone0")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -177,6 +177,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
