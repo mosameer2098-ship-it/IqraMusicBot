3d73fb4c5f8e1d07dc210f4769965ff904c0a968
Sameer <mosameer2098@gmail.com>
2026-08-15 21:59:05 +0530

    Update support and channel links

--- config.py
+++ config.py
@@ -49,8 +49,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/iqra_music_support")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/best_friends_chatting_zone0")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AdsNovaPro_Official")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/iqraa_music_support")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
